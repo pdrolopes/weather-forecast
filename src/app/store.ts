925b887a7233896ad25f8100de2a716a4a2e3ff3
Pedro Lopes de Azevedo <ordepi@gmail.com>
2021-06-04 16:02:08 -0300

    Added region slice to redux store

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,9 +1,11 @@
 import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
 import counterReducer from '../features/counter/counterSlice';
+import regionReducer from '../features/region/regionSlice';
 
 export const store = configureStore({
   reducer: {
     counter: counterReducer,
+    region: regionReducer,
   },
 });
 
