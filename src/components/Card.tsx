7d4100e9b8d29f6248449fcc4296bb822681650c
Pedro Lopes de Azevedo <ordepi@gmail.com>
2021-06-05 17:58:42 -0300

    Created card component

---
+++ src/components/Card.tsx
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+const Card = styled.div`
+  background-color: white;
+  box-shadow: 0 2px 6px -2px gray;
+  padding: 8px;
+  border-radius: 4px;
+`
+
+export default Card;
