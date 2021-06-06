565db295bd343d886c6ba55f13fd1f3bc1841d6c
Pedro Lopes de Azevedo <ordepi@gmail.com>
2021-06-06 17:06:00 -0300

    Updated readme

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,28 +1,28 @@
 module.exports = {
-    parser: '@typescript-eslint/parser',
-    parserOptions: {
-      ecmaVersion: 2020,
-      sourceType: 'module',
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: 'module',
+  },
+  settings: {
+    react: {
+      version: 'detect',
     },
-    settings: {
-      react: {
-        version: 'detect',
-      },
-    },
-    globals: {
-      jest: true,
-    },
-    extends: [
-      'react-app',
-      'plugin:react/recommended',
-      'plugin:@typescript-eslint/recommended',
-      'prettier/@typescript-eslint',
-      'plugin:prettier/recommended',
-      'eslint:recommended',
-    ],
-    rules: {
-      'no-use-before-define': 'off',
-      '@typescript-eslint/no-use-before-define': ['off'],
-      '@typescript-eslint/explicit-function-return-type': ['off'],
-    },
-  };
+  },
+  globals: {
+    jest: true,
+  },
+  extends: [
+    'react-app',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier/@typescript-eslint',
+    'plugin:prettier/recommended',
+    'eslint:recommended',
+  ],
+  rules: {
+    'no-use-before-define': 'off',
+    '@typescript-eslint/no-use-before-define': ['off'],
+    '@typescript-eslint/explicit-function-return-type': ['off'],
+  },
+};
